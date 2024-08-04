9311e36f55b9f8966afe85b7d7223db6de2e13ea
jmonger8888 <joshuamonrra@gmail.com>
2024-08-04 13:17:42 -0600

    basedatos

---
+++ ProyectoFinalMovil/ProyectoFinalMovil/RepositoryDB.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Firebase.Database;
+using Newtonsoft.Json;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Xamarin.Essentials;
+
+namespace ProyectoFinalMovil
+{
+    public class RepositoryDB
+    {
+        FirebaseClient firebaseClient = new
+            FirebaseClient("https://proyectofinalmovil-ed3f4-default-rtdb.firebaseio.com");
+    }
+}
